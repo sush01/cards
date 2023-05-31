5c26146a6656e232b85e275017609f7098a12fed
sush01 <kandel.sushila242@gmail.com>
2023-05-31 23:09:48 +0300

    New card component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cards';
+
+  posts = [
+    {
+      title:  `Nightview`,
+      imageUrl: `assets/nightview.jpeg`,
+      username: `nature`,
+      content: `Awesome view from appartment balcony`
+   },
+   {
+     title:  `Sushila`,
+     imageUrl: `assets/sush.jpeg`,
+     username: `mypic`,
+     content: `Photo from my friend´s wedding`
+  },
+  { 
+    title:  `Windmill`,
+    imageUrl: `assets/windmill.jpeg`,
+    username: `renewable energy`,
+    content: `Picture of windmill from the shore`
+   }
+  ];
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
+ @Input() title = ``;
+ @Input() imageUrl = ``;
+ @Input() content = ``;
+ @Input() username = ``;
+ 
+  constructor(){}
+
+  ngOnInit() {}
 
 }
+
